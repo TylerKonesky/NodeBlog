566bce9ed4a08b4e37122a5f65ac3faf21821482
TylerKonesky <tylerkonesky@hotmail.com>
2020-07-24 10:38:29 -0600

    removed keys

--- config/dev.js
+++
@@ -1,7 +0,0 @@
-module.exports = {
-  googleClientID:
-  '157308862747-8h6j4ue6e6ah8rukrmfplgpjlgmcl1ke.apps.googleusercontent.com',
-  googleClientSecret: 'LGsxWyZTFTpxRkDdZib1MN4T',
-  mongoURI: 'mongodb://TylerK:JlzFzPCrPy4mamzf@cluster0-shard-00-00.khzbx.mongodb.net:27017,cluster0-shard-00-01.khzbx.mongodb.net:27017,cluster0-shard-00-02.khzbx.mongodb.net:27017/Cluster0?ssl=true&replicaSet=atlas-xht96f-shard-0&authSource=admin&retryWrites=true&w=majority',
-  cookieKey: '123123123',
-};
