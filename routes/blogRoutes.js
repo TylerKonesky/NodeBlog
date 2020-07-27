12102bc5a514a138ea401f2a3488686ca9b54824
TylerKonesky <tylerkonesky@hotmail.com>
2020-07-27 15:11:08 -0600

    fixed but and setup delete cache

--- routes/blogRoutes.js
+++ routes/blogRoutes.js
@@ -1,6 +1,8 @@
 const mongoose = require('mongoose');
 const requireLogin = require('../middlewares/requireLogin');
-const {clearHash} = require('../services/cache');
+const clearCache = require('../middlewares/cleanCache');
+const cleanCache = require('../middlewares/cleanCache');
+
 const Blog = mongoose.model('Blog');
 
 module.exports = app => {
@@ -14,13 +16,12 @@ module.exports = app => {
   });
 
   app.get('/api/blogs', requireLogin, async (req, res) => {
-    const blogs = await Blog.find({_user: req.user.id}).cache();
-    console.log(blogs)
+    const blogs = await Blog.find({_user: req.user.id}).cache({key: req.user.id});
     res.send(blogs)
 
   });
 
-  app.post('/api/blogs', requireLogin, async (req, res) => {
+  app.post('/api/blogs', requireLogin, cleanCache, async (req, res) => {
     const { title, content } = req.body;
 
     const blog = new Blog({
@@ -35,6 +36,5 @@ module.exports = app => {
     } catch (err) {
       res.send(400, err);
     }
-    clearHash(req.user.id)
   });
 };
