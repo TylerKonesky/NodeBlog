45a9b78e16c42d3c2e603e50d8154d2c3eb865af
TylerKonesky <tylerkonesky@hotmail.com>
2020-08-08 15:13:17 -0600

    Travis config

---
+++ config/ci.js
@@ -0,0 +1,8 @@
+module.exports = {
+    googleClientID:
+    '157308862747-8h6j4ue6e6ah8rukrmfplgpjlgmcl1ke.apps.googleusercontent.com',
+    googleClientSecret: 'LGsxWyZTFTpxRkDdZib1MN4T',
+    mongoURI: 'mongodb://127.0.0.1:27017/blog_ci',
+    cookieKey: '123123123',
+    redisUrl: 'redis://127.0.0.1:6379'
+  };
