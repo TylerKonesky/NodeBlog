1edc9005a5d90f7dc032532b6a2b1aa52730d155
TylerKonesky <tylerkonesky@hotmail.com>
2020-08-09 23:07:00 -0600

    AWS

---
+++ routes/uploadRoutes.js
@@ -0,0 +1,12 @@
+const AWS = require('aws-sdk');
+const keys = require('../config/keys');
+
+const s3 = new AWS.S3({
+    accessKeyId: keys.accessKeyId,
+    secretAccessKey: keys.secretAccessKey
+})
+module.exports = app  =>{
+    app.get('/api/uplodad', (req, res) => {
+
+    })
+}
